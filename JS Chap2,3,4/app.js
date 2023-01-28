d8bb4e8a761ce190874fe7f451e4a9f0dcd1964b
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-01-28 15:29:46 +0500

    Add files via upload

---
+++ JS Chap2,3,4/app.js
@@ -0,0 +1,71 @@
+            // Chap#2
+
+// Question#1
+// var username=;
+
+// Question#2
+// var myName="Mustafa Faisal"
+
+// Question#3
+// var massage="Hello World"
+// alert(massage)
+
+// Question#4
+// var name="Jhone Doe"
+// var age="15 Years Old"
+// var work="Certified Mobile App Developer"
+// alert(name)
+// alert(age)
+// alert(work)
+
+// Question#5
+// var pizza="pizza\npizz\npiz\npi\np"
+// alert(pizza)
+
+// Question#6
+// var email="faisalmustafa067@gmail.com"
+// alert("my email address is "+ email)
+
+// Question#7
+// var book="A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book "+book)
+
+// Question#8
+// document.write("<p>Yah! I can write HTML content through java script </p>")
+
+// Question#9
+// var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
+              // Chap#3
+
+// Question#1
+// var age=15
+// alert("I am "+age+" years old")
+
+// Question#2
+// var noOfVisitors=14
+// alert("You have visited this site" +noOfVisitors+ " times")
+
+// Question#3
+// var birthYear=1990
+// document.write("<p>My birth year is "+birthYear+"</p> <p> Data type of my decleared variable is number</p>")
+
+// Question#4
+// var visitorName="John Doe"
+// var producTitle=" T-shirt"
+// var quantity=5
+// document.write("<p>"+visitorName+" ordered "+quantity+producTitle+"on XYZ Clothing Store ")
+
+            // Chap#4
+
+// Question#1
+// var name="Mustafa", father="faisal", age=19
+
+// Question#3
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("<p>Variable names can only contain number $ _ and Character</p>")
+// document.write("<p>Variables must begin with a letter $ or _. For example $name, _name or name</p>")
+// document.write("<p>Variable names are case sensitive</p>")
+// document.write("<p>Variable names should not be JS Keywords</p>")
+
